0272867732cac9ef999c08586ace5adf23294983
Atharva Jadhav <42040257+atharva21jadhav@users.noreply.github.com>
2020-05-17 12:32:36 +0530

    Create Simple Array Sum.cpp

---
+++ HackerRank/Simple Array Sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    vector <int> inputVector;
+    int n,i, inputElement,sum=0;
+    cin>>n;
+    for(i=0;i<n;i++){
+        cin>>inputElement;
+        inputVector.push_back(inputElement);
+    }
+    for(i=0;i<n;i++){
+        sum=sum+ inputVector.at(i);
+    }
+    cout<<sum;
+    return 0;
+}
